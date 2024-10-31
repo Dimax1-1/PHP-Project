ed641cc0912cceb9ab9f1b9a1db3ed1a0e7d050b
Dimax1-1 <115618293+Dimax1-1@users.noreply.github.com>
2024-10-31 11:17:59 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configurazione del database PostgreSQL
-$db_host = 'localhost'; // L'host del database
-$db_name = 'Progetto'; // Il nome del database
-$db_user = 'postgres'; // Il nome utente del database
-$db_password = '123456'; // La password del database
-?>
